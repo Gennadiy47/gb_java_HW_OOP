8fdfb1d907108b2984828d8e1682b90d71cead4c
GENNADIY MATVIENKO <gennady39@yandex.ru>
2023-08-01 18:29:49 +0300

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,29 @@
+ public class Human extends Visitor{
+
+        super(name);
+    }
+
+    @Override
+    String getName() {
+        return super.name;
+    }
+
+    @Override
+    public void setMakeOrder(boolean status) {
+    }
+
+    @Override
+    public void setTakeOrder(boolean status) {
+    }
+
+    @Override
+    public boolean isMakeOrder() {
+        return false;
+    }
+
+    @Override
+    public boolean isTakeOrder() {
+        return false;
+    }
+
+}
